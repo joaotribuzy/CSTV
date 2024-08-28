29235657428d858a637340b2a8fda54d6b19aa73
João Tribuzy <github@joaotribuzy.com>
2024-08-28 14:34:21 -0400

    Initial Commit

---
+++ CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by João Tribuzy on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
