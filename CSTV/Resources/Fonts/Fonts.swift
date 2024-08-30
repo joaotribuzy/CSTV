d7ef4c7c82d5d016677b299f5abd059803fec387
João Tribuzy <github@joaotribuzy.com>
2024-08-30 10:44:04 -0400

    feat: Add the Roboto custom font

---
+++ CSTV/Resources/Fonts/Fonts.swift
@@ -0,0 +1,26 @@
+//
+//  Fonts.swift
+//  CSTV
+//
+//  Created by João Tribuzy on 29/08/24.
+//
+
+import SwiftUI
+
+enum Fonts {
+    static let teamFlagTitle = customFont(size: 12)
+    static let versusTitle = customFont(size: 14)
+    static let leagueDescription = customFont(size: 10)
+    static let timeLabel = customFont(size: 10, weight: .bold)
+    
+    private static func customFont(size: CGFloat, weight: Font.Weight? = nil) -> Font {
+        switch weight {
+            case .medium:
+                return Font.custom("Roboto-Medium", size: size)
+            case .bold:
+                return Font.custom("Roboto-Bold", size: size)
+            default:
+                return Font.custom("Roboto-Regular", size: size)
+        }
+    }
+}
